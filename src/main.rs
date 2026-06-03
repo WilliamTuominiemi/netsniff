cd164b0cbf396847ec25bbaacea31a0ce2ce49ad
williamtuominiemi <william.m.tuominiemi@gmail.com>
2026-06-03 21:50:56 +0300

    timestamp from header into readable format

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+use chrono::{DateTime, Local, Utc};
 use etherparse::SlicedPacket;
-use pcap::{Device, Packet};
+use pcap::{Device, Packet, PacketHeader};
 
 use std::io;
 
@@ -15,8 +16,8 @@ fn main() {
         .unwrap();
 
     while let Ok(packet) = cap.next_packet() {
-        println!("Packet received: {:?}", packet.header);
-        parse_packet(packet);
+        parse_time(packet.header);
+        // parse_packet(packet);
     }
 }
 
@@ -38,6 +39,18 @@ fn configuration(devices: Vec<Device>) -> Device {
         .expect("Please enter a valid number");
 
     devices.into_iter().nth(index).expect("Index out of range")
+}
+
+fn parse_time(header: &PacketHeader) {
+    let timestamp = header.ts;
+    let nanos = (timestamp.tv_usec as u32) * 1_000;
+
+    if let Some(dt) = DateTime::from_timestamp(timestamp.tv_sec as i64, nanos) {
+        let utc_time: DateTime<Utc> = dt;
+        let local_time: DateTime<Local> = DateTime::from(utc_time);
+        let naive_time = local_time.time();
+        println!("[{}]: ", naive_time);
+    }
 }
 
 fn parse_packet(packet: Packet) {
