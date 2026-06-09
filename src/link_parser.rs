752d26c5017fbc1ec0ecbf048cacf715ebd07d2d
williamtuominiemi <william.m.tuominiemi@gmail.com>
2026-06-09 19:20:15 +0300

    list source and target MAC address

--- src/link_parser.rs
+++ src/link_parser.rs
@@ -1,4 +1,4 @@
-use etherparse::{LinkSlice, SlicedPacket};
+use etherparse::{Ethernet2Slice, LinkSlice, SlicedPacket};
 
 pub struct LinkParser<'a> {
     pub link: Option<LinkSlice<'a>>,
@@ -8,12 +8,20 @@ impl<'a> LinkParser<'a> {
     pub fn parse(&self) {
         match &self.link {
             Some(link_slice) => match link_slice {
-                LinkSlice::Ethernet2(slice) => println!("{:?}", slice),
-                LinkSlice::LinuxSll(slice) => println!("{:?}", slice),
-                LinkSlice::EtherPayload(slice) => println!("{:?}", slice),
-                LinkSlice::LinuxSllPayload(slice) => println!("{:?}", slice),
+                LinkSlice::Ethernet2(slice) => self.parse_ethernet_2_slice(slice),
+                LinkSlice::LinuxSll(slice) => println!("LinuxSll"),
+                LinkSlice::EtherPayload(slice) => println!("EtherPayload"),
+                LinkSlice::LinuxSllPayload(slice) => println!("LinuxSllPayload"),
             },
             _ => panic!(),
         }
+    }
+
+    fn parse_ethernet_2_slice(&self, slice: &Ethernet2Slice) {
+        println!(
+            "  Ethernet2 MAC {:X?} -> {:X?} ",
+            slice.source(),
+            slice.destination()
+        );
     }
 }
