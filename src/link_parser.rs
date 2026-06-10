2eeb3ccb66aa162977b28136be59647882a59bb0
williamtuominiemi <william.m.tuominiemi@gmail.com>
2026-06-10 21:39:22 +0300

    format MAC address properly

--- src/link_parser.rs
+++ src/link_parser.rs
@@ -1,3 +1,5 @@
+use std::result;
+
 use etherparse::{Ethernet2Slice, LinkSlice, SlicedPacket};
 
 pub struct LinkParser<'a> {
@@ -19,9 +21,16 @@ impl<'a> LinkParser<'a> {
 
     fn parse_ethernet_2_slice(&self, slice: &Ethernet2Slice) {
         println!(
-            "  Ethernet2 MAC {:X?} -> {:X?} ",
-            slice.source(),
-            slice.destination()
+            "  Ethernet2 MAC {} -> {} ",
+            self.mac_as_array_to_string(&slice.source()),
+            self.mac_as_array_to_string(&slice.destination())
         );
+    }
+
+    fn mac_as_array_to_string(&self, mac: &[u8]) -> String {
+        mac.iter()
+            .map(|byte| format!("{:02X}", byte))
+            .collect::<Vec<String>>()
+            .join(":")
     }
 }
